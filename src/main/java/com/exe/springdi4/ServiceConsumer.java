d10ec50f25e084ed93d6ca3315d12d47725dcef3
Miny1110 <wjdalswjd453@naver.com>
2022-08-26 13:59:56 +0900

    0826 오후 프로젝트 복사

--- src/main/java/com/exe/springdi4/ServiceConsumer.java
+++ src/main/java/com/exe/springdi4/ServiceConsumer.java
@@ -1,41 +1,29 @@
 package com.exe.springdi4;
 
-public class ServiceConsumer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+/** @Component : 객체 생성 */
+@Component("serviceConsumer")
+public class ServiceConsumer {
+	
+	/** @Autowired : 의존성 주입
+	 * 	(객체가 생성되었다는 조건) */
+	
+	@Autowired
+	@Qualifier("messageService")
 	MessageService ms;
+	
+	@Autowired
 	TimeService ts;
+	
+	@Autowired
 	JobService js;
 	
-	/**메소드로 의존성 주입을 하기 위해서는 기본생성자 선언이 꼭 필요*/
-	public ServiceConsumer() {}
-	
-	/**[생성자로 의존성 주입]*/
-	public ServiceConsumer(MessageService ms) {
-		this.ms = ms;
-	}
-	
-	/**[메소드로 의존성 주입]
-	 * 
-	 * 메소드로 의존성 주입을 할 때에는 기본생성자가 꼭 필요해서 위에서 선언
-	 * 기본생성자로 객체생성을 한 후에 메소드가 실행되기 때문
-	 * 
-	 * app-context.xml 에서 매개변수를 넣어줘야 실행됨*/
-	public void setTimeService(TimeService ts) {
-		this.ts = ts;
-	}
-
-	public void setJobService(JobService js) {
-		this.js = js;
-	}
-
 	
 	public void consumerService() {
-		
-//		GenericXmlApplicationContext context = 
-//				new GenericXmlApplicationContext("app-context.xml");
-//		
-//		MessageService ms = (MessageService)context.getBean("messageService");
-		
+
 		String message = ms.getMessage();
 		System.out.println(message);
 		
